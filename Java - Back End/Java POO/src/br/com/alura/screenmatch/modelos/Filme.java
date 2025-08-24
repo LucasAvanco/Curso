3142895a3b13c563f16db45d8437b453c206efc4
Lucas Avanco <lucasavancodasilva@gmail.com>
2025-08-24 14:33:39 -0300

    again

---
+++ Java - Back End/Java POO/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme extends Titulo {
+    private string Diretor;
+
+    public string getDiretor() {
+        return Diretor;
+    }
+
+    public void setDiretor(string diretor) {
+        Diretor = diretor;
+    }
+}
--- Java - Back End/Java POO/src/br/com/alura/screenmatch/modelos/Series.java
+++ Java - Back End/Java POO/src/br/com/alura/screenmatch/modelos/Series.java
@@ -1,4 +1,41 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Series {
+public class Series extends Titulo{
+    private int temporadas;
+    private int epsodiosPorTemporada;
+    private boolean ativa;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpsodiosPorTemporada() {
+        return epsodiosPorTemporada;
+    }
+
+    public void setEpsodiosPorTemporada(int epsodiosPorTemporada) {
+        this.epsodiosPorTemporada = epsodiosPorTemporada;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
 }
